68856d8cb1585d4caf4bdbcbeef848da0b163d87
Victor Chiong <cjvicro@gmail.com>
2024-11-12 16:22:55 +0800

    Dashboard and Appointment Page of Patient

---
+++ resources/js/Layouts/PatientLayout.jsx
@@ -0,0 +1,121 @@
+import { Link, usePage } from '@inertiajs/react';
+import React from 'react';
+import { PiAddressBookBold } from 'react-icons/pi';
+import { RiCalendarTodoLine } from 'react-icons/ri';
+import Dropdown from '@/Components/Inputs/Dropdown';
+import { FaGripLines } from 'react-icons/fa';
+
+const logo = "/assets/svg/logo.svg";
+const header = "/assets/svg/header.svg";
+
+export default function PatientLayout({children}) {
+
+    const {url, component} = usePage();
+    const user = usePage().props.auth.user;
+    return (
+        <div className='min-h-screen h-full flex flex-col'>
+            <div className="header bg-cover p-5 text-white text-center" style={{ backgroundImage: `url(${header})`, height: '109px' }}>
+                <div className="flex justify-between items-center">
+                    <Link href={route('patient.dashboard')}>
+                        <img src={logo} alt="Logo" className="w-24" />
+                    </Link>
+                    <div className="search-bar flex items-center space-x-2">
+                        <input
+                        type="text"
+                        className="form-control text-black px-4 py-2 w-[300px] rounded-lg border border-gray-300"
+                        placeholder="Search"
+                        />
+                        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg">Search</button>
+                    </div>
+                    <div className="relative ms-3">
+                        <Dropdown>
+                            <Dropdown.Trigger>
+                                <button>
+                                    <img src="profile-icon.png" alt="Profile" className="w-12 h-12 rounded-full bg-gray-200" />
+                                </button>
+                            </Dropdown.Trigger>
+
+                            <Dropdown.Content>
+                                {
+                                    [
+                                        {text: "Profile", href: route('patient.view.profile', user.id)},
+                                        {text: "Change Password", href: route('patient.view.password', user.id)},
+                                        {text: "Log Out", href: route('logout', user.id)},
+                                    ].map( (link, idx) => {
+                                        return (
+                                            <Dropdown.Link
+                                                key={idx}
+                                                href={link.href}
+                                            >
+                                                {link.text}
+                                            </Dropdown.Link>
+                                        )
+                                    })
+                                }
+                            </Dropdown.Content>
+                        </Dropdown>
+                    </div>
+                </div>
+            </div>
+    
+            <section className='flex h-full grow'>
+                <div className="flex flex-col flex-[1_0_15%] bg-secondary-bg text-center flex justify-center">
+                    {
+                        [
+                            {icon: <RiCalendarTodoLine />,text: "Book Appointments", href: route('patient.book.appointments'), route: '/patient/book/appointments', sublinks: []},
+                            {icon: <RiCalendarTodoLine />,text: "Community", href: route('patient.show.communities'), route: '/patient/show/communities',
+                                sublinks: [
+                                    {text: "Patients", href: route('patient.show.communities'), route: '/patient/show/communities'},
+                                    {text: "Practitioners", href: route('patient.show.communities'), route: '/patient/show/practitioners'},
+                                    {text: "BHWs", href: route('patient.show.communities'), route: '/patient/show/bhws'},
+                                ]
+                            },
+                            {icon: <RiCalendarTodoLine />,text: "Service Available", href: route('patient.show.service.availables'), route: '/patient/show/service/availables',
+                                sublinks: [
+                                    {text: "Schedule Consultations", href: route('patient.show.communities'), route: '/patient/show/communities'},
+                                    {text: "Medicine Available", href: route('patient.show.communities'), route: '/patient/show/communities'},
+                                    {text: "Data Analysis Reports", href: route('patient.show.communities'), route: '/patient/show/communities'},
+                                    {text: "BHW Activities", href: route('patient.show.communities'), route: '/patient/show/communities'},
+                                ]
+                            },
+                            {icon: <PiAddressBookBold />,text: "My Records", href: route('patient.show.record.medicals'), route: '/patient/show/record/medicals',
+                                sublinks: [
+                                    {text: "My Medical Records", href: route('patient.show.communities'), route: '/patient/show/communities'},
+                                    {text: "My Medical History", href: route('patient.show.communities'), route: '/patient/show/communities'},
+                                ]
+                            },
+                        ].map( (link, idx) => {
+                            const active = url === link.route;
+                            return (
+                                <>
+                                <Link key={idx} href={link.href} className={`flex items-center gap-4 px-4 py-2 ${url === link.route ? 'text-white' : 'hover:text-white'} transition`}>
+                                    {link.icon}
+                                    <span>{link.text}</span>
+                                </Link>
+                                <ul className={`${active ? 'visible' : 'hidden'}`}>
+                                {
+                                    link.sublinks.map( (sub, idx) => {
+                                        return (
+                                            <li key={idx}>
+                                                <Link key={idx} href={link.href} className={`flex items-center gap-4 px-8 py-2 ${url === sub.route ? 'text-white' : 'hover:text-white'} transition`}>
+                                                    <FaGripLines />
+                                                    <span>{sub.text}</span>
+                                                </Link>
+                                            </li>
+                                        )
+                                    })
+                                }
+                                </ul>
+                                </>
+                            )
+                        })
+                    }
+                </div>
+                
+                <main className='p-6 flex-[1_0_85%]'>
+                    {children}
+                </main>
+            </section>
+        </div>
+    );
+}
--- resources/js/Pages/Patients/Appointments/Appointment.jsx
+++ resources/js/Pages/Patients/Appointments/Appointment.jsx
@@ -1,9 +1,216 @@
-import React from 'react'
+import React, { Suspense, useState } from 'react'
+import { Head } from '@inertiajs/react';
+import PatientLayout from '@/Layouts/PatientLayout'
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction';
 
 const Appointment = () => {
-  return (
-    <div>Patient Appointment</div>
-  )
+    const [showModal, setShowModal] = useState(false);
+    const [selectedAppointment, setSelectedAppointment] = useState(null);
+    const bookings = [{
+      title: 'General Consultation',
+      notes: 'Sample Descriptions',
+      appointment_date: '2024-11-11',
+      doctor_name: 'Victor Chiong',
+      patient_name: 'Victor Wawa',
+      appointment_start: '09:00:00.000Z',
+      appointment_end: '05:00:00.000Z',
+      booking_status: 'Success',
+    },{
+        title: 'Dentism',
+        notes: 'Sample Descriptions',
+        appointment_date: '2024-11-15',
+        doctor_name: 'Victor Chiong',
+        patient_name: 'Victor Wawa',
+        appointment_start: '09:00:00.000Z',
+        appointment_end: '05:00:00.000Z',
+        booking_status: 'Pending',
+    }];
+
+    const completedBookings = bookings.filter( booking => booking.booking_status === "Success");
+    const latestFinishedBookings = completedBookings.length > 0 ? completedBookings[completedBookings.length - 1] : null;
+
+    const formatTime = (isoString) => {
+        const date = new Date(isoString);
+        return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true });
+    };
+    
+    const bookingSchedule = bookings.map(booking => {
+        const startTime = `${booking.appointment_date}T${booking.appointment_start}`;
+        const endTime = `${booking.appointment_date}T${booking.appointment_end}`;
+    
+        return {
+            title: booking.title,
+            start: startTime,
+            end: endTime,      
+            
+            extendedProps: {
+                date: booking.appointment_date,
+                status: booking.booking_status,
+                doctor_name: booking.doctor_name,
+                patient_name: booking.patient_name,
+                notes: booking.notes,
+                formattedStart: formatTime(startTime),
+                formattedEnd: formatTime(endTime)
+            }
+        };
+    });
+
+    const handleEventClick = (info) => {
+        const { date, status, doctor_name, patient_name, notes } = info.event.extendedProps;
+        setSelectedAppointment({
+            title: info.event.title,
+            start: info.event.start,
+            end: info.event.end,
+            date: date,
+            status: status,
+            doctor_name: doctor_name,
+            patient_name: patient_name,
+            notes: notes,
+        });
+        setShowModal(true);
+    };
+    
+
+    const closeModal = () => {
+        setShowModal(false);
+        setSelectedAppointment(null);
+    };
+
+    const renderEvent = (eventInfo) => {
+      const status = eventInfo.event._def.extendedProps.status;
+      const title = eventInfo.event._def.title;
+      return (
+        <span className='relative items-center overflow-hidden text-center'>
+          <div className={`absolute top-1 left-1 rounded-full w-2 h-2 ${status === "Success" ? 'bg-app-complete' : 'bg-app-coming'}`}></div>
+          <span className='pl-4'>{title}</span>
+        </span>
+      )
+    }
+
+    const barangayEvent = {
+      title: 'Dental and General Checkup',
+      date_start: '2024-04-29T00:00:00.000Z',
+      date_end: '2024-04-29T15:00:00.000Z',
+      location: 'BHW Center',
+      doctor_name: 'Victor Chiong',
+      bhw_name: 'Victor BHW'
+    }
+
+    const localeOptions = { timeZone: 'Asia/Manila', hour: '2-digit', minute: '2-digit', hour12: true}
+
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <PatientLayout>
+                <Head title="Scheduling" />
+
+                <div className="grid grid-cols-12 h-screen">
+                    <div className="col-span-7">
+                        <FullCalendar
+                            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                            initialView="dayGridMonth"
+                            eventContent={renderEvent}
+                            headerToolbar={{
+                                start: "today, prev, next",
+                                center: "title",
+                                // end: ""
+                            }}
+                            events={bookingSchedule}
+                            selectable={true}
+                            buttonText={{
+                                today: 'Today',
+                                month: 'Month',
+                                week: 'Week',
+                                day: 'Day'
+                            }}
+                        />
+                        <div className='flex justify-center gap-16 mt-4'>
+                          <span className='flex gap-2 items-center'>
+                            <div className='w-4 h-4 bg-app-complete'></div>
+                            Completed
+                          </span>
+                          <span className='flex gap-2 items-center'>
+                            <div className='w-4 h-4 bg-app-coming'></div>
+                            Coming
+                          </span>
+                        </div>
+                        {
+                          latestFinishedBookings && (
+                            <div className='flex flex-col justify-center items-center mt-8'>
+                              <span>Previous Sched: {new Date(latestFinishedBookings.appointment_date).toLocaleDateString('en-US', {
+                                year: 'numeric',
+                                month: 'long',
+                                day: 'numeric',
+                              })}</span>
+                              <span>{latestFinishedBookings.title} by Dr. {latestFinishedBookings.doctor_name}</span>
+                            </div>
+                          )
+                        }
+                        
+                    </div>
+
+                    <div className="col-span-5 flex flex-col gap-4 p-4">
+                      {
+                        barangayEvent && (
+                          <section className='p-8 '>
+                            <div className='relative z-10'>
+                              <div className='w-full h-full absolute bg-secondary-bg top-8 left-8 rounded-md z-10 '></div>
+                              <div className='relative border-2 rounded-md border-black bg-white p-4 z-50'>
+                                {
+                                  [
+                                    {head: "WHAT", item: barangayEvent.title},
+                                    {head: "WHEN", item: `${new Date(barangayEvent.date_start).toLocaleString('en-US', {
+                                      timeZone: 'Asia/Manila',
+                                      year: 'numeric',
+                                      month: 'long',
+                                      day: 'numeric',
+                                    })} ${new Date(barangayEvent.date_start).toLocaleTimeString('en-US',localeOptions)} - ${new Date(barangayEvent.date_end).toLocaleTimeString('en-PH',localeOptions)}`},
+                                    {head: "WHERE", item: barangayEvent.location},
+                                    {head: "DOC In-Charge", item: `Dr. ${barangayEvent.doctor_name} MD`},
+                                    {head: "BHW In-Charge", item: barangayEvent.bhw_name},
+                                  ].map( (itm, idx) => {
+                                    return (
+                                      <div key={idx} className='flex flex-col'>
+                                        <span>{itm.head}:</span>
+                                        <span className='pl-8'>{itm.item}</span>
+                                      </div>
+                                    )
+                                  })
+                                }
+                                <div className='flex justify-center mt-8'>
+                                  <button className='bg-secondary-bg hover:bg-primary-bg hover:text-white transition px-8 py-4 rounded-full border-2 border-black'>
+                                    Book Appointment
+                                  </button>
+                                </div>
+                              </div>
+                            </div>
+                          </section>
+                        )
+                      }
+                      <FullCalendar
+                          plugins={[listPlugin]}
+                          initialView="listWeek"
+                          events={bookingSchedule}
+                          selectable={true}
+                          eventClick={handleEventClick}
+                          headerToolbar={false}
+                      />            
+                    </div>
+                </div>
+            </PatientLayout>
+
+            {/* {showModal && (
+                <AppointmentModal
+                    showModal={showModal}
+                    toggleModal={closeModal}
+                    selectedAppointment={selectedAppointment}
+                />
+            )} */}
+        </Suspense>
+    );
 }
 
 export default Appointment
