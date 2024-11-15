cb462369fb9158deba0bc0955b6fe9d9d0ebfb72
MarvsTech <115031416+Crispkreme@users.noreply.github.com>
2024-11-15 18:22:37 +0800

    Update Event.jsx

--- resources/js/Components/Cards/Event.jsx
+++ resources/js/Components/Cards/Event.jsx
@@ -16,7 +16,7 @@ const Event = ({ event }) => {
 
     return (
         <div className="flex flex-col md:flex-row gap-3 bg-white border border-gray-300 rounded-xl overflow-hidden items-center justify-start">
-            <div className="flex items-center justify-center w-full h-32 flex-shrink-0 bg-blue-100 text-4xl font-bold text-blue-600">
+            <div className="flex items-center justify-center w-full md:w-32 h-32 flex-shrink-0 bg-blue-100 text-4xl font-bold text-blue-600">
                 {day}
             </div>
             <div className="flex flex-col gap-2 py-2">
