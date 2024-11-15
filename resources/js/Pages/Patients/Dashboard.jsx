86f30bca0b05c227b709fa8c9fdb1c8b2ca04845
Victor Chiong <cjvicro@gmail.com>
2024-11-15 16:22:51 +0800

    added chart

--- resources/js/Pages/Patients/Dashboard.jsx
+++ resources/js/Pages/Patients/Dashboard.jsx
@@ -1,8 +1,40 @@
 import StandardLayout from '@/Layouts/StandardLayout';
 import { Head } from '@inertiajs/react';
 import PatientLayout from '@/Layouts/PatientLayout';
+import Chart from "chart.js/auto";
+import { Bar } from 'react-chartjs-2';
 
 export default function Dashboard() {
+
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  const commonIlness = [
+    {name: "Cough", percent: 46},
+    {name: "Flu", percent: 23},
+    {name: "Fever", percent: 38},
+  ]
+  const labels = commonIlness.map( il => il.name);
+  const data = {
+      labels: ["Common Ilness"],
+      datasets: commonIlness.map( c => {
+        return {
+          label: c.name,
+          backgroundColor: getRandomColor(),
+          borderColor: "rgb(255, 99, 132)",
+          data: [c.percent],
+        }
+      }),
+    };
+    
+
+  
     return (
         <PatientLayout>
             <Head title="Dashboard" />
@@ -15,8 +47,7 @@ export default function Dashboard() {
 
             <div className="container mx-auto flex flex-col md:flex-row justify-around items-start mt-6 space-y-4 md:space-y-0 md:space-x-4">
                 <div className="chart-card bg-gray-50 rounded-lg p-6 text-center shadow-lg w-full md:w-1/2">
-                    <h6 className="text-lg font-semibold">Common Illness</h6>
-                    <p className="text-gray-600">Cough - 46%<br />Flu - 38%</p>
+                  <Bar data={data}/>
                 </div>
                 <div className="chart-card bg-gray-50 rounded-lg p-6 text-center shadow-lg w-full md:w-1/2">
                     <h6 className="text-lg font-semibold">In-Demand Medicine</h6>
