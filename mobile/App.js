a5b3f19a0d188af2e9e82ceb0e1eb7d4842878f8
MarvsTech <115031416+Crispkreme@users.noreply.github.com>
2024-12-29 16:41:54 +0800

    updated

--- mobile/App.js
+++ mobile/App.js
@@ -1,20 +1,45 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { AuthProvider } from "./contexts/AuthContext";
 
 import LoginScreen from "./screens/LoginScreen";
 import PatientHomeScreen from "./screens/PatientHomeScreen";
 
+const Drawer = createDrawerNavigator();
 const Stack = createNativeStackNavigator();
+
+function DrawerNavigator({ route }) {
+  const user = route?.params?.user;
+
+  return (
+    <Drawer.Navigator initialRouteName="Dashboard">
+      <Drawer.Screen 
+        name="Dashboard" 
+        component={PatientHomeScreen} 
+        initialParams={{ user }}
+        options={{ headerShown: false }}
+      />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <AuthProvider>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-          <Stack.Screen name="PatientHomeScreen" component={PatientHomeScreen} options={{ headerShown: false }} />
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen 
+            name="Login" 
+            component={LoginScreen} 
+            options={{ headerShown: false }} 
+          />
+          <Stack.Screen 
+            name="Dashboard"
+            component={DrawerNavigator} 
+            options={{ headerShown: false }} 
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </AuthProvider>
